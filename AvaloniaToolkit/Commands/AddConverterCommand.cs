68d377bd77f3230e177eb48c0d210835c8b2c658
Dong Bin <popmessiah@hotmail.com>
2022-05-22 14:47:11 +0800

    feat: Initialize project.

---
+++ AvaloniaToolkit/Commands/AddConverterCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace AvaloniaToolkit
+{
+    [Command(PackageGuids.AvaloniaToolkitString, PackageIds.AddConverterCommand)]
+    internal sealed class AddConverterCommand : BaseCommand<AddConverterCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("AddConverterCommand", "Button clicked");
+        }
+    }
+}
---
+++ AvaloniaToolkit/ViewModels/AddConverterDialogViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaToolkit.ViewModels
+{
+    internal class AddConverterDialogViewModel
+    {
+    }
+}
